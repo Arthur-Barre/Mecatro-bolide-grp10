07026950ee04ab8c5b5bd8e2cade2fa89136558f
Milan François <milan.francois@etu.minesparis.psl.eu>
2025-11-05 11:54:44 +0100

    main telemetry

--- Arduino/main_telemetry.m
+++ Arduino/main_telemetry.m
@@ -8,13 +8,13 @@ port_name = "COM8";
 baudrate = 230400;
 
 %Recording time (s)
-Tmax = 4.5;
+Tmax = 20;
 
 %Communication method
 method = 'WiFi';
 
 %Gains
-K = [1 2 3];
+K = [1 1 1];  % dans l'ordre K_moins, Kp, Ki
 
 if strcmp(method,'WiFi')
     [log_time, data_values, line_idx] = get_data_WiFi(IP, PORT_NUMBER, Tmax, K);
@@ -27,19 +27,26 @@ end
 %Display logged variable names
 disp(data_values.keys)  
 
-% Plot logged variables
-% figure; 
-% hold on;
-% legend_labels = data_values.keys;
-% for i = 1:numel(legend_labels)
-%     %if legend_labels{i}=="SensorBar" 
-%     if legend_labels{i}=="Omega Droite" || legend_labels{i}=="angleDroite" %|| legend_labels{i}=="speed"
-%         d = legend_labels{i};
-%         v = data_values(d);
-%         %plot(log_time, v, 'DisplayName', d);
-%     end
-% end
-% hold off;
-% legend('Location', 'best');
-% 
-% grid on;
+%Plot logged variables
+figure; 
+hold on;
+legend_labels = data_values.keys;
+for i = 1:numel(legend_labels)
+    %if legend_labels{i}=="SensorBar" 
+    if legend_labels{i}=="u" || legend_labels{i}=="dlambda" || legend_labels{i}=="Omega Droite" || legend_labels{i}=="Omega Gauche"
+        d = legend_labels{i};
+        v = data_values(d);
+        plot(log_time, v, 'DisplayName', d);
+    end
+end
+hold off;
+legend('Location', 'best');
+grid on;
+
+figure
+hold on;
+plot(log_time, data_values("U_plus"), 'DisplayName', "U_plus");
+plot(log_time, data_values("U_moins"), 'DisplayName', "U_moins");
+hold off;
+legend('Location', 'best');
+grid on;
